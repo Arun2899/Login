a91db54595af6054c3a323dbf182f7de0f7d4572
Arun2899 <arunnishanthan28@gmail.com>
2025-01-07 06:40:53 +0530

    Update LoginApplication.java

--- src/main/java/com/example/Login/LoginApplication.java
+++ src/main/java/com/example/Login/LoginApplication.java
@@ -11,6 +11,7 @@ public class LoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
+		System.out.println("Update from git");
 	}
 
 }
